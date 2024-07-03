b40c2cddd5f70e5990311cbf41712b0941c62c61
Ace-Kz <ahato0808@gmail.com>
2024-07-03 22:22:54 +0900

    勝ち負け表示追加

--- ex3.c
+++ ex3.c
@@ -3,8 +3,14 @@
 #include <time.h>
 
 int main() {
-
- srand(time(NULL));
+    
+char name[20];
+    
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("Hello,%s!",name);
+    
+    srand(time(NULL));
     int dice1 = (rand() % 6) + 1;
     int dice2 = (rand() % 6) + 1;
 
@@ -12,5 +18,12 @@ int main() {
     printf("Die 1: %d\n",dice1);
     printf("Die 2: %d\n",dice2);
     printf("Total value: %d\n",dice1 + dice2);
-
+    
+    if(dice1 + dice2 <= 8){
+    printf("%s won!\n",name);
+    }
+    else{
+    printf("%s lose!\n",name);
+    }
+    return 0;
 }
